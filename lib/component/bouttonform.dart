89fc7aa0b824a25300a5a6d3cf75edd38819ba5b
ZephirSD <siegfrieddallery@gmail.com>
2022-01-26 20:35:14 +0400

    Création des composants

---
+++ lib/component/bouttonform.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class BouttonForm extends StatelessWidget {
+  final Color backgroundButton;
+  final String texteBoutton;
+  final Widget test;
+  BouttonForm(this.backgroundButton, this.texteBoutton, this.test);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: TextButton(
+        onPressed: () => {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => test,
+            ),
+          ),
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(
+            backgroundButton,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Text(
+            texteBoutton,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
